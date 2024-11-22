eecda41f3b8aadd5bf609d0c03a32b039039409e
aymen <adirar@figma.com>
2024-11-21 20:26:49 -0800

    a

--- copy.sh
+++ copy.sh
@@ -3,7 +3,7 @@ trash .vscode/*
 
 cp -r ~/.config/kitty .config
 cp -r ~/.config/nvim .config
-mkdir -p .config/tmux && cp ~/.config/tmux .config/tmux
+mkdir -p .config/tmux && cp -r ~/.config/tmux .config/tmux
 
 cp ~/Library/Application\ Support/Code/User/settings.json .vscode
 cp ~/Library/Application\ Support/Code/User/keybindings.json .vscode
