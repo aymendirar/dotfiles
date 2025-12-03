098ab8c28826f3afb97d626dbbf0be842c76e661
aymen <adirar@figma.com>
2025-12-03 14:28:50 -0500

    update

--- .config/nvim/lua/plugins/lazy/bufferline.lua
+++ .config/nvim/lua/plugins/lazy/bufferline.lua
@@ -2,15 +2,14 @@ return {
   "akinsho/bufferline.nvim",
   -- version = "v3.*",
   event = "VeryLazy",
-  dependencies = "nvim-tree/nvim-web-devicons",
+  dependencies = { "nvim-tree/nvim-web-devicons", "catppuccin" },
   opts = {
     options = {
       mode = "buffers",
     },
   },
-  config = function()
-    require("bufferline").setup({
-      highlights = require("catppuccin.special.bufferline").get_theme(),
-    })
+  init = function()
+    local bufline = require("catppuccin.groups.integrations.bufferline")
+    bufline.get = bufline.get_theme
   end,
 }
