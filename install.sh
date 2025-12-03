5a52e94e5a4e19fd2838b91a42b84fb7edf54c99
aymen <adirar@figma.com>
2025-12-03 13:26:45 -0500

    update

--- install.sh
+++ install.sh
@@ -12,15 +12,9 @@ install_fzf() {
   ~/.fzf/install --all
 }
 
-install_nvim() {
-  curl -o ~/nvim-linux-x86_64/bin/nvim "https://github.com/neovim/neovim/releases/download/v0.11.5/nvim-linux-x86_64.tar.gz"
-  tar xzvf nvim-linux-x86_64.tar.gz
-}
-
-install_fzf
-install_nvim
-
-sudo apt install neovim
+if ! fzf --version ; then
+  install_fzf
+fi
 
 if [ -d "~/.config/nvim" ]; then
   rm -rf ~/.config/nvim
